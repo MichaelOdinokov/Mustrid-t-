6c2b09f0398e5ed83cf324dd2e9ece8fb4861ab5
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-03-21 20:47:27 +0200

    12

--- Mustrid.py
+++ Mustrid.py
@@ -39,15 +39,12 @@ x0=0
 y0=0
 x1=80
 y1=80
-a=40
-r=(a**2+a**2)**(1/2)
-p=(a-r)
 
 for i in range(1):
-    x0+=p
-    y0+=p
-    x1-=p
-    y1-=p
+    x0+=80
+    y0+=80
+    x1-=80
+    y1-=80
     tahvel.create_rectangle(x0,y0,x1,y1,width=1, fill="white")
 
 x0=0
@@ -55,36 +52,33 @@ y0=0
 x1=80+160 #90+160
 y1=80
 for i in range(1):
-    x0+=p
-    y0+=p
-    x1-=p
-    y1-=p
+    x0+=80
+    y0+=80
+    x1-=80
+    y1-=80
     tahvel.create_rectangle(x0,y0,x1,y1,width=1, fill="black")
 
 #Harjutus. Muster oval 2
-
-colors=["black","cyan","magneta", "red", "blue","gray","yellow","green","lightblue","pink","gold"]
-ramm2=Tk()
-ramm2.title("Ringid")
-tahvel2 = Canvas(raam, width=600, height=600, background="white")
-
+colors= ["black","cyan","magenta","red","blue","gray","yellow","green","lightblue","pink","gold"]
+raam2=Tk()
+raam2.title("ringid")
+tahvel2=Canvas(raam2,width=600,height=600,background="white")
 x0=0
 y0=0
 x1=600
 y1=600
 p=5
 for i in range(55):
-    x0+=p
-    y0+=p
-    x1-=p
-    y1-=p
-    tahvel2.create_oval(x0,y0,x1,y1, fill=choice(colors))
+    x0+=p 
+    y0+=p 
+    x1-=p 
+    y1-=p 
+    tahvel2.create_oval(x0,y0,x1,y1,fill=choice(colors))
+    tahvel2.grid()
 
 
 
 
 
 tahvel.grid()
-tahvel2.grid()
-ramm2.mainloop()
 raam.mainloop()
